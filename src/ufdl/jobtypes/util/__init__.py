6f8b2759c5519b442c68499b38915b4c76f3ecb9
csterling <coreytsterling@gmail.com>
2022-01-24 12:44:18 +1300

    Made format_type_arg_or_param public.

--- src/ufdl/jobtypes/util/__init__.py
+++ src/ufdl/jobtypes/util/__init__.py
@@ -1,3 +1,3 @@
-from ._format import format_type_or_type_class
+from ._format import format_type_or_type_class, format_type_arg_or_param
 from ._is_subtype import is_subtype
 from ._parse import parse_type
