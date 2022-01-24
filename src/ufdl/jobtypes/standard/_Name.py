9117c9bb3d34b14abca5775bf095f4c8b8771021
csterling <coreytsterling@gmail.com>
2022-01-25 10:09:04 +1300

    Added type for extracting names.

---
+++ src/ufdl/jobtypes/standard/_Name.py
@@ -0,0 +1,30 @@
+from typing import Tuple, Type
+
+from wai.json.raw import RawJSONElement
+from wai.json.schema import JSONSchema, enum
+
+from ..base import UFDLJSONType, NamedServerType
+
+
+class Name(UFDLJSONType[Tuple[NamedServerType], str]):
+    def parse_json_value(self, value: RawJSONElement) -> str:
+        if not isinstance(value, str):
+            raise ValueError("Not a string")
+        return value
+
+    def format_python_value_to_json(self, value: str) -> RawJSONElement:
+        return value
+
+    @property
+    def json_schema(self) -> JSONSchema:
+        sub_type = self.type_args[0]
+        return enum(
+            *(
+                sub_type.extract_name(value)
+                for value in sub_type.list_all_values()
+            )
+        )
+
+    @classmethod
+    def type_params_expected_base_types(cls) -> Tuple[Type[NamedServerType]]:
+        return NamedServerType,
--- src/ufdl/jobtypes/standard/__init__.py
+++ src/ufdl/jobtypes/standard/__init__.py
@@ -1,3 +1,4 @@
 from ._JobOutput import JobOutput
 from ._Model import Model
+from ._Name import Name
 from ._PK import PK
