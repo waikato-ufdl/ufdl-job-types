420b8b5393264115402e1596e82a2f33e982555a
csterling <coreytsterling@gmail.com>
2022-01-24 16:27:26 +1300

    Exposed format_type_args_or_params.

--- src/ufdl/jobtypes/util/__init__.py
+++ src/ufdl/jobtypes/util/__init__.py
@@ -1,4 +1,4 @@
-from ._format import format_type_or_type_class, format_type
+from ._format import format_type_or_type_class, format_type, format_type_args_or_params
 from ._is_subtype import is_subtype
 from ._is_ufdl_type import is_ufdl_type
 from ._parse import parse_type
