4b7bc626a59f72c158b82630d4414c63810f5ee2
csterling <coreytsterling@gmail.com>
2022-01-25 14:24:37 +1300

    Fixed typing error.

--- src/ufdl/jobtypes/initialise/_not_initialised.py
+++ src/ufdl/jobtypes/initialise/_not_initialised.py
@@ -1,9 +1,7 @@
-from typing import Any, Callable, NoReturn
-
 from ..error import NotInitialisedException
 
 
-def not_initialised() -> Callable[[Any, ...], NoReturn]:
+def not_initialised():
     """
     Default for all initialisation-required functions which throws an error
     stating that the function has not been initialised.
