ea894ae0cfcffa53d920d59219c67b8cc44d7c80
csterling <coreytsterling@gmail.com>
2022-02-01 11:10:14 +1300

    Removed unused values package.

--- src/ufdl/jobtypes/values/_Empty.py
+++
@@ -1,6 +0,0 @@
-from ._Enumerable import Enumerable, ValueType
-
-
-class Empty(Enumerable[ValueType]):
-    def __init__(self):
-        super().__init__()
--- src/ufdl/jobtypes/values/_ValueSet.py
+++
@@ -1,7 +0,0 @@
-from typing import Generic, TypeVar
-
-ValueType = TypeVar('ValueType')
-
-
-class ValueSet(Generic[ValueType]):
-    pass
