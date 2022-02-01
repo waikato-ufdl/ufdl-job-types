a9e1c6ae2c73b11116a9ea50b34c64c81637daba
csterling <coreytsterling@gmail.com>
2022-02-02 11:13:37 +1300

    Made name finite.

--- src/ufdl/jobtypes/standard/_Name.py
+++ src/ufdl/jobtypes/standard/_Name.py
@@ -1,29 +1,30 @@
-from typing import Tuple, Type
+from typing import List, Tuple
 
 from wai.json.raw import RawJSONElement
 from wai.json.schema import JSONSchema, enum
 
-from ..base import UFDLJSONType, NamedServerType, UFDLType
+from ..base import FiniteJSONType, NamedServerType, UFDLType
 
 
-class Name(UFDLJSONType[Tuple[NamedServerType], str]):
+class Name(FiniteJSONType[Tuple[NamedServerType], str]):
     def parse_json_value(self, value: RawJSONElement) -> str:
-        if not isinstance(value, str):
-            raise ValueError("Not a string")
+        self.validate_with_schema(value)
         return value
 
     def format_python_value_to_json(self, value: str) -> RawJSONElement:
+        self.validate_with_schema(value)
         return value
+
+    def list_all_values(self) -> List[RawJSONElement]:
+        sub_type = self.type_args[0]
+        return list(
+            sub_type.extract_name(value)
+            for value in sub_type.list_all_values()
+        )
 
     @property
     def json_schema(self) -> JSONSchema:
-        sub_type = self.type_args[0]
-        return enum(
-            *(
-                sub_type.extract_name(value)
-                for value in sub_type.list_all_values()
-            )
-        )
+        return enum(*self.list_all_values())
 
     @classmethod
     def type_params_expected_base_types(cls) -> Tuple[UFDLType, ...]:
