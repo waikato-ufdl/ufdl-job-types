584fb2b2e945166f8a8b50b708ae242de72c4159
csterling <coreytsterling@gmail.com>
2022-02-15 12:51:51 +1300

    Added stringly-typed parameter to BLOB.

--- src/ufdl/jobtypes/standard/util/_BLOB.py
+++ src/ufdl/jobtypes/standard/util/_BLOB.py
@@ -1,11 +1,11 @@
-from typing import IO, Tuple, Union
+from typing import IO, Optional, Tuple, Union, overload
 
-from ...base import UFDLType
+from ...base import UFDLType, String
 
 
 class BLOB(
     UFDLType[
-        tuple,
+        Tuple[String],
         Union[bytes, IO[bytes]],
         Union[bytes, IO[bytes]]
     ]
@@ -13,6 +13,17 @@ class BLOB(
     """
     Utility type for raw binary values.
     """
+    @overload
+    def __init__(self, type_hint: str): ...
+    @overload
+    def __init__(self, type_args: Optional[Tuple[String]] = None): ...
+
+    def __init__(self, *args):
+        if len(args) == 1 and isinstance(args[0], str):
+            args = String.generate_subclass(args[0]),
+
+        super().__init__(*args)
+
     def parse_binary_value(self, value: Union[bytes, IO[bytes]]) -> Union[bytes, IO[bytes]]:
         return value
 
@@ -21,7 +32,7 @@ class BLOB(
 
     @classmethod
     def type_params_expected_base_types(cls) -> Tuple[UFDLType, ...]:
-        return tuple()
+        return String(),
 
     @property
     def is_abstract(self) -> bool:
