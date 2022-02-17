562ff80e80e9f8c050ca602e67537515e5d2e30b
csterling <coreytsterling@gmail.com>
2022-02-18 12:04:25 +1300

    Added instance type for docker images.

--- src/ufdl/jobtypes/standard/server/_DockerImage.py
+++ src/ufdl/jobtypes/standard/server/_DockerImage.py
@@ -1,23 +1,94 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple, Type, overload
 
 from ufdl.json.core.filter import FilterExpression
 from ufdl.json.core.filter.field import Exact
+from wai.json.object import OptionallyPresent, StrictJSONObject
+from wai.json.object.property import (
+    ArrayProperty,
+    BoolProperty,
+    NumberProperty,
+    StringProperty
+)
 from wai.json.raw import RawJSONElement, RawJSONObject
-from wai.json.schema import JSONSchema, standard_object, number, string_schema
+from wai.json.schema import JSONSchema
 
 from ...base import NamedServerType, UFDLType
+from ...error import expect
 from ...util import parse_v_name
+from ._CUDAVersion import CUDAVersionInstance
 from ._Domain import Domain
-from ._Framework import Framework
+from ._Framework import Framework, FrameworkInstance
+from ._Hardware import HardwareInstance
+
+
+class DockerImageInstance(StrictJSONObject['DockerImageInstance']):
+    """
+    The value-type of the DockerImage UFDL-type.
+    """
+    # The primary-key of the model, if it was sent by the server
+    pk: OptionallyPresent[int] = NumberProperty(integer_only=True, minimum=1, optional=True)
+
+    name: OptionallyPresent[str] = StringProperty(optional=True)
+
+    version: OptionallyPresent[str] = StringProperty(optional=True)
+
+    url: str = StringProperty()
+
+    registry_url: str = StringProperty()
+
+    registry_username: OptionallyPresent[str] = StringProperty(optional=True)
+
+    registry_password: OptionallyPresent[str] = StringProperty(optional=True)
+
+    cuda_version: CUDAVersionInstance = CUDAVersionInstance.as_property()
+
+    framework: FrameworkInstance
+
+    domain: str
+
+    tasks: List[str] = ArrayProperty(
+        element_property=StringProperty()
+    )
+
+    min_hardware_generation = HardwareInstance.as_property()
+
+    cpu: bool = BoolProperty()
+
+    licence: OptionallyPresent[str] = StringProperty(optional=True)
 
 
 class DockerImage(
     NamedServerType[
         Tuple[Domain, Framework],
-        RawJSONObject,
-        RawJSONObject
+        DockerImageInstance,
+        DockerImageInstance
     ]
 ):
+    @overload
+    def __init__(self, domain_type: Domain, framework_type: Framework): ...
+    @overload
+    def __init__(self, type_args: Optional[Tuple[Domain, Framework]] = None): ...
+
+    def __init__(self, *args):
+        if len(args) == 2:
+            args = args,
+        super().__init__(*args)
+        self._instance_class: Optional[Type[DockerImageInstance]] = None
+
+    @property
+    def instance_class(self):
+        if self._instance_class is None:
+            domain_type = self.type_args[0]
+            framework_type = self.type_args[1]
+
+            class SpecialisedDockerImageInstance(DockerImageInstance):
+                framework = framework_type.instance_class.as_property()
+                domain = domain_type.instance_class.description
+
+            self._instance_class = SpecialisedDockerImageInstance
+
+        return self._instance_class
+
     def name_filter(self, name: str) -> FilterExpression:
         name, version = parse_v_name(name)
         return Exact(field="name", value=name) & Exact(field="version", value=version)
@@ -44,23 +115,16 @@ class DockerImage(
 
         return rules
 
-    def parse_json_value(self, value: RawJSONElement) -> RawJSONObject:
-        self.validate_with_schema(value)
-        return value
+    def parse_json_value(self, value: RawJSONElement) -> DockerImageInstance:
+        return self.instance_class.from_raw_json(value)
 
-    def format_python_value_to_json(self, value: RawJSONObject) -> RawJSONElement:
-        self.validate_with_schema(value)
-        return value
+    def format_python_value_to_json(self, value: DockerImageInstance) -> RawJSONElement:
+        expect(self.instance_class, value)
+        return value.to_raw_json()
 
     @property
     def json_schema(self) -> JSONSchema:
-        return standard_object(
-            {
-                "pk": number(minimum=1, integer_only=True),
-                "name": string_schema(max_length=64),
-                "version": string_schema(max_length=32)
-            }
-        )
+        return self.instance_class.get_json_validation_schema()
 
     @classmethod
     def type_params_expected_base_types(cls) -> Tuple[UFDLType, ...]:
--- src/ufdl/jobtypes/standard/server/__init__.py
+++ src/ufdl/jobtypes/standard/server/__init__.py
@@ -1,6 +1,6 @@
 from ._CUDAVersion import CUDAVersion, CUDAVersionInstance
 from ._Dataset import Dataset
-from ._DockerImage import DockerImage
+from ._DockerImage import DockerImage, DockerImageInstance
 from ._Domain import Domain, DomainInstance
 from ._Framework import Framework, FrameworkInstance
 from ._Hardware import Hardware, HardwareInstance
